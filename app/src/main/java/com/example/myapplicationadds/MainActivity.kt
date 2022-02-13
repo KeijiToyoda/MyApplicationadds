1734e544a1940836a19426713d3dde333beb684f
orbid018 <tomo09250626@icloud.com>
2022-02-13 18:02:16 +0900

    komitto

--- app/src/main/java/com/example/myapplicationadds/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationadds/MainActivity.kt
@@ -1,46 +1,39 @@
 package com.example.myapplicationadds
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlin.concurrent.timer
 import android.os.Bundle
-import android.widget.RelativeLayout
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.example.myapplicationadds.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mAdView : AdView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val AdUnitID = "ca-app-pub-1640470884247283/9130371672"
+        lateinit var binding: ActivityMainBinding
+        lateinit var mAdView : AdView
+        val AdUnitID = "ca-app-pub-3940256099942544/6300978111"
+        val roulette:roulette = findViewById(R.id.roulette)
 
         // Relative layout インスタンス生成
-        val layout = RelativeLayout(this)
-        setContentView(layout)
+        MobileAds.initialize(this){}
 
-        val params = RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.MATCH_PARENT,
-            RelativeLayout.LayoutParams.WRAP_CONTENT
-        )
-
-        // AdMob インスタンス生成
-        val adMobView = AdView(this)
-        adMobView.setAdUnitId(AdUnitID)
-        adMobView.setAdSize(AdSize.BANNER)
-        // AdMobをレイアウトに追加
-        layout.addView(adMobView)
-
-        // AdMobのレイアウト属性を設定
-        adMobView.setLayoutParams(params)
-
-        // AdMobをロードして表示
-        MobileAds.initialize(this) {}
 
         mAdView = findViewById(R.id.adView)
+        // AdMobをロードして表示
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
-    }
-}
 
+        }
+
+    }
---
+++ app/src/main/java/roulette.kt
@@ -0,0 +1,69 @@
+package com.example.myapplicationadds
+
+import android.content.Context
+import android.graphics.*
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlin.concurrent.timer
+
+class roulette (context: Context?, var color: Int) : View(context) {
+    private var paint: Paint = Paint()
+    var deg:Float = 0F
+    var n = 6
+    var degm:Float = 0F
+    var state = 0
+    var txt:String = "START"
+    val colLis = listOf(
+        Color.argb(200,200,125,125),
+        Color.argb(200,125,125,200),
+        Color.argb(200,125,200,125),
+        Color.argb(200,120,200,200),
+        Color.argb(200,200,200,120),
+        Color.argb(200,200,120,200))
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        super.onTouchEvent(event)
+        if(state == 0) {
+            state = 1
+            degm = 179F
+            txt = "STOP"
+        }else if(state == 1){
+            state = 2
+            txt = "WAIT"
+        }
+        return super.onTouchEvent(event)
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        val wid = width.toFloat()
+        val height = height.toFloat()
+        super.onDraw(canvas)
+        canvas?.drawColor(color)
+        paint.setColor(colLis[2])
+        val re = RectF(wid/2-300F,height/2-300F,wid/2+300F,height/2+300F)
+        for(i in 0..n-1){
+            paint.setColor(colLis[i%n])
+            var rad:Float = 360/n.toFloat()
+            canvas?.drawArc(re,(rad*i+deg)%360,rad,true,paint)
+            paint.setColor(Color.WHITE)
+        }
+        canvas?.drawCircle(wid/2,height/2,200F,paint)
+        paint.setColor(Color.BLACK)
+        paint.textAlign = Paint.Align.CENTER
+        paint.textSize = 100F
+        canvas?.drawText(txt,wid/2,height/2,paint)
+        deg += degm
+        if(state == 2)degm -= 2F
+        if(degm <= 0F){
+            degm = 0F
+            state = 0
+            txt = "START"
+        }
+        invalidate()
+    }
+}
